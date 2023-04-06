ea38543b96746bfd47d414bfbec830c5d7c0451e
Fady Nakhla <fnakhla23@gmail.com>
2023-04-05 17:13:37 -0700

    updating feedforward

--- transformers/feedforward/feedforward.py
+++ transformers/feedforward/feedforward.py
@@ -8,13 +8,15 @@ from transformers.util_layers import layernorm
 class FeedForward(nn.Module):
     def __init__(self, hidden_size: int, dropout: float = 0.1):
         super().__init__()
-        self.layers = nn.ModuleList()
-        self.layers.append(nn.Linear(hidden_size, hidden_size))
-        self.layers.append(nn.Dropout(dropout))
-        self.layers.append(nn.Linear(hidden_size, hidden_size))
-        self.layer_norm = layernorm.LayerNorm(hidden_size)
+        layers = [
+            nn.Linear(hidden_size, hidden_size),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_size, hidden_size),
+        ]
+        self.layers = nn.ModuleList(layers)
 
     def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        output = hidden_states
         for layer in self.layers:
-            hidden_states = layer(hidden_states)
-        return self.layer_norm(hidden_states)
+            output = layer(output)
+        return output
