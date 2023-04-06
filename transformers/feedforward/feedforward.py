6f239977890f69dd2ac8700f773f27f35b5fd213
Fady Nakhla <fnakhla23@gmail.com>
2023-04-05 17:16:36 -0700

    option to norm input of ff

--- transformers/feedforward/feedforward.py
+++ transformers/feedforward/feedforward.py
@@ -6,7 +6,7 @@ from transformers.util_layers import layernorm
 
 
 class FeedForward(nn.Module):
-    def __init__(self, hidden_size: int, dropout: float = 0.1):
+    def __init__(self, hidden_size: int, dropout: float = 0.1, normalize_inputs: bool = False):
         super().__init__()
         layers = [
             nn.Linear(hidden_size, hidden_size),
@@ -14,9 +14,12 @@ class FeedForward(nn.Module):
             nn.Linear(hidden_size, hidden_size),
         ]
         self.layers = nn.ModuleList(layers)
+        self.layer_norm = layernorm.LayerNorm(hidden_size) if normalize_inputs else None
 
     def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
         output = hidden_states
+        if self.layer_norm:
+            output = self.layer_norm(output)
         for layer in self.layers:
             output = layer(output)
         return output
