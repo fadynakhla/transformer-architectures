307abb0f47469b801434a58487901b12ebaba857
Fady Nakhla <fnakhla23@gmail.com>
2026-02-22 20:18:15 -0800

    fix async grad logging

--- transformer_architectures/training/grad_logging.py
+++ transformer_architectures/training/grad_logging.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 import mlflow
 import torch
 import torch.nn as nn
+from mlflow import MlflowClient
 
 
 def log_gradients_and_params(
+    client: MlflowClient,
+    run_id: str,
     model_params: dict[str, torch.Tensor],
     model_grads: dict[str, torch.Tensor],
     step: int,
@@ -17,7 +20,7 @@ def log_gradients_and_params(
         fig, ax = plt.subplots()
         ax.hist(param.flatten().numpy(), bins=64)
         ax.set_title(name)
-        mlflow.log_figure(fig, f"params/{name}_step_{step}.png")
+        client.log_figure(run_id, fig, f"params/{name}_step_{step}.png")
         plt.close(fig)
 
         if (grad_name := f"grad/{name}") in model_grads:
@@ -26,17 +29,20 @@ def log_gradients_and_params(
             fig, ax = plt.subplots()
             ax.hist(grads.flatten().numpy(), bins=64)
             ax.set_title(f"grad/{name}")
-            mlflow.log_figure(fig, f"grads/{name}_step_{step}.png")
+            client.log_figure(run_id, fig, f"grads/{name}_step_{step}.png")
             plt.close(fig)
 
             grad_norm = float(torch.norm(grads))
             total_grad_norm += grad_norm**2
 
     total_grad_norm = total_grad_norm**0.5
-    mlflow.log_metric("grad_norm", total_grad_norm, step=step, synchronous=False)
+    client.log_metric(run_id, "grad_norm", total_grad_norm, step=step)
 
 
 def async_log(model: nn.Module, step: int) -> None:
+    run_id = mlflow.active_run().info.run_id
+    client = MlflowClient()
+
     model_params: dict[str, torch.Tensor] = {}
     model_grads: dict[str, torch.Tensor] = {}
 
@@ -50,5 +56,5 @@ def async_log(model: nn.Module, step: int) -> None:
 
     threading.Thread(
         target=log_gradients_and_params,
-        args=(model_params, model_grads, step),
+        args=(client, run_id, model_params, model_grads, step),
     ).start()
