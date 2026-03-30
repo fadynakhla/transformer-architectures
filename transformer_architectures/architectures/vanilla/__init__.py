2f07230595d2bed32e62081535f1e3203b45e7f8
Fady <fnakhla23@gmail.com>
2026-03-30 05:58:35 +0000

    removing single device training pathway for vanilla transformer

--- transformer_architectures/architectures/vanilla/__init__.py
+++ transformer_architectures/architectures/vanilla/__init__.py
@@ -1,8 +1,8 @@
 from transformer_architectures.architectures.vanilla.data import (
     LabeledBatch,
     SourceTarget,
-    TransformerDataModule,
 )
+from transformer_architectures.architectures.vanilla.datamodule import TransformerDataModule
 from transformer_architectures.architectures.vanilla.tokenization import Tokenizer
 from transformer_architectures.architectures.vanilla.transformer import Transformer
 
